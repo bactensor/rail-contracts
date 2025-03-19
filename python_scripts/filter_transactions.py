3544b40c537cf6a56f8606db7d259ba4acf03597
Mateusz Srebrny <mateusz.srebrny@reef.pl>
2025-03-19 12:47:04 +0100

    filter doesn't need abi, only the contract address

--- python_scripts/filter_transactions.py
+++ python_scripts/filter_transactions.py
@@ -5,7 +5,6 @@ from web3 import Web3
 import sys
 
 NUMBER_OF_RECENT_BLOCKS_TO_CHECK = 256
-DEPLOYED_CONTRACT_FILE_PATH = '../deployed-contract.json'
 OUTPUT_FILE = 'transactions.csv'
 BOUNDED_SIGNATURE = 'checkpointBounded(bytes32)'
 UNBOUNDED_SIGNATURE = 'checkpointUnbounded(bytes)'
@@ -18,13 +17,8 @@ BYTES_TO_SKIP = {
 w3 = Web3(Web3.HTTPProvider('https://evm-testnet.dev.opentensor.ai'))
 
 
-def main(signature):
+def main(contract_address, signature):
     assert signature in [BOUNDED_SIGNATURE, UNBOUNDED_SIGNATURE], f"Invalid signature: {signature}"
-
-    address = None
-    with open(DEPLOYED_CONTRACT_FILE_PATH, 'r') as file:
-        deployed_contract = json.load(file)
-        address = deployed_contract['address']
 
     # Compute the function selector from the signature
     function_selector = Web3.keccak(text=signature)[:4].to_0x_hex()
@@ -41,7 +35,7 @@ def main(signature):
         for block_number in range(starting_block_num, ending_block_num + 1):
             block = w3.eth.get_block(block_number, full_transactions=True)
             for tx in block.transactions:
-                if tx['to'] == address and tx['input'].to_0x_hex()[:10] == function_selector:
+                if tx['to'] == contract_address and tx['input'].to_0x_hex()[:10] == function_selector:
                     argument = tx['input'].to_0x_hex()[BYTES_TO_SKIP[signature]:]  
                     sender = tx['from']
 
@@ -51,9 +45,11 @@ def main(signature):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or sys.argv[1] not in ['bounded', 'unbounded']:
-        print("Usage: python filter_transactions.py <bounded|unbounded>")
+    if len(sys.argv) != 3 or sys.argv[2] not in ['bounded', 'unbounded']:
+        print("Usage: python filter_transactions.py <contract_address> <bounded|unbounded>")
         sys.exit(1)
 
-    signature = BOUNDED_SIGNATURE if sys.argv[1] == "bounded" else UNBOUNDED_SIGNATURE
-    main(signature)
+    contract_address = sys.argv[1]
+    
+    signature = BOUNDED_SIGNATURE if sys.argv[2] == "bounded" else UNBOUNDED_SIGNATURE
+    main(contract_address, signature)
